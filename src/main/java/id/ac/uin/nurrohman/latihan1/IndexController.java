6f0ea5fe129eeb98b76393b8e1fdc861063403ec
omvn10 <100698149+Nurrohman10@users.noreply.github.com>
2022-03-09 21:04:25 +0700

    Initial commit

---
+++ src/main/java/id/ac/uin/nurrohman/latihan1/IndexController.java
@@ -0,0 +1,16 @@
+package id.ac.uin.nurrohman.latihan1;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+
+public class IndexController {
+    @GetMapping("home")
+    public String home(@RequestParam String nama){return "Halo, selamat datang " + nama + " ";
+    }
+}
